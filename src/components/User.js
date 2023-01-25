10c00f5788b8b06dbb9dabc8a8d73d5b08b82d60
wjdals95 <wjdals992@naver.com>
2023-01-25 22:02:26 +0900

    User컴포넌트를 class컴포넌트로 변경

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,16 @@
-import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from './User.module.css';
+// porps같은 여러 속성을 추가하기위해 리액트에서 Component를 가져와서 아래처럼 extends Component를 추가해줘서 확장시킨다.
+
+class User extends Component{
+  render () {
+    // props를 사용할때 예약어 this와 함께 써야한다.
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
